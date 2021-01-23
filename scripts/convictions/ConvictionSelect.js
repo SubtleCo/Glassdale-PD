005635ee8a24c9b992004a095f426b186519524b
Alex Martin <alex@subtleco.co>
2021-01-22 22:33:38 -0600

    Added Conviction Selection files, used .map() to parse conviction objects into array, then again to create the list of options

---
+++ scripts/convictions/ConvictionSelect.js
@@ -0,0 +1,20 @@
+import { getConvictions, useConvictions } from './ConvictionProvider.js';
+
+const contentTarget = document.querySelector(".filters__crime");
+
+export const ConvictionSelect = () => {
+    getConvictions()
+    .then( () => {
+        const convictions = useConvictions()
+        render(convictions)
+    })
+}
+
+const render = convictionsCollection => {
+    contentTarget.innerHTML = `
+        <select class="dropdown" id="crimeSelect">
+            <option value="0">Select a crime...</option>
+            ${
+                convictionsCollection.map(conviction => `<option>${conviction}</option>}`)
+            }`
+}
